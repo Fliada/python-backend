37acbe8c0a8406480b1af89b90bd0a415e75ac39
Tyan v sharfe <thisonlystudy@gmail.com>
2023-12-06 16:39:57 +0500

    Add create order

--- api/data/request.py
+++ api/data/request.py
@@ -1,3 +1,8 @@
+from api.db.DBHelper import DBHelper
+
+helper = DBHelper()
+
+
 class Request:
     def __init__(self, id_, user_id, staff_id, address_id, comment, status_id,
                  date_creation, date_selected, date_actual):
@@ -10,3 +15,22 @@ class Request:
         self.date_creation = date_creation
         self.date_selected = date_selected
         self.date_actual = date_actual
+
+
+def create_request(user_id, staff_id, address_id, comment, status_id,
+                   date_creation, date_selected, date_actual):
+    params = ["user_id", "staff_id", "address_id", "comment", "status_id",
+              "date_creation", 'date_selected', 'date_actual']
+
+    args = [
+        user_id,
+        staff_id,
+        address_id,
+        comment,
+        status_id,
+        date_creation,
+        date_selected,
+        date_actual
+    ]
+
+    helper.insert("request", params, args)
