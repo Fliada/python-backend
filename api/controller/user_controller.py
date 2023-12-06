265a817db3db8f76b7138844c7fa2fd7a6d52372
Alina <wbrflf3301@outlook.com>
2023-12-06 14:49:44 +0500

    insert user

--- api/controller/user_controller.py
+++ api/controller/user_controller.py
@@ -1,4 +1,8 @@
 from flask import Blueprint
+from flask import Flask, request
+from api.data.user import create_user, get_user
+import requests
+
 
 user = Blueprint('user_routes', __name__)
 
@@ -13,6 +17,23 @@ def get_user(user_id):
 @user.route('/user/<int:user_id>', methods=['PUT'])
 def put_user(user_id):
     return 'Put user with Id %d' % user_id
+
+
+# TODO СДЕЛАТЬ РЕАЛИЗАЦИЮ
+@user.route('/user/create', methods=['POST'])
+def insert_user():
+    data = request.get_json()
+
+    password = data.get('password')
+    is_superuser = data.get('is_superuser')
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+    second_name = data.get('second_name')
+    is_staff = data.get('is_staff')
+    is_active = data.get('is_active')
+    email = data.get('email')
+    create_user(password, is_superuser, first_name, last_name, second_name, is_staff, is_active, email)
+    return 'Create user'
 
 
 # TODO СДЕЛАТЬ РЕАЛИЗАЦИЮ БАНА ПОЛЬЗОВАТЕЛЯ
