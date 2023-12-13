c177e2285f9a10eb17b3efaeb03fb1f8d787e2ff
Tyan v sharfe <thisonlystudy@gmail.com>
2023-12-13 20:00:11 +0500

    Work

--- api/data/material.py
+++ api/data/material.py
@@ -63,4 +63,4 @@ def delete_material(_id):
 
 def toJSON(self):
     return json.dumps(self, default=lambda o: o.__dict__,
-                      sort_keys=True, indent=4)
+                      sort_keys=True, indent=4, ensure_ascii=False)
